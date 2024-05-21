d525a653cccc1d3ccfc40a04970021e7aa192970
Maso44 <143696681+Maso44@users.noreply.github.com>
2024-05-21 09:36:49 +0200

    dikek update

--- activities/index.php
+++ activities/index.php
@@ -11,6 +11,14 @@ $currentPage = 'activities';
 include("../config.php");
 include("../userincludes/userfunctions.inc.php");
 include("../db/dbh.inc.php");
+
+// Query om de benodigde gegevens op te halen
+$sql = "SELECT user.userFirstname, user.userLastname, assignment.assignmentName, activity.totalTime 
+        FROM user 
+        JOIN activity ON user.userId = activity.userId 
+        JOIN assignment ON activity.assignmentId = assignment.assignmentId";
+
+$result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +31,27 @@ include("../db/dbh.inc.php");
     <link rel="stylesheet" href="activities.css">
     <link rel="stylesheet" href="../assets/layout.css">
     <link rel="stylesheet" href="../assets/navbar.css">
+    <script>
+    function myFunction() {
+      var input, filter, table, tr, td, i, txtValue;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
 
+      for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }       
+      }
+    }
+    </script>
 </head>
 
 <body>
@@ -32,9 +60,28 @@ include("../db/dbh.inc.php");
 
         <div class="dashboard-wrapper">
             <div class="dashboard-window">
+                <?php
+                if ($result->num_rows > 0) {
+                  // Maak een zoekbalk
+                  echo "<input type='text' id='myInput' onkeyup='myFunction()' placeholder='Zoek naar namen..'>";
+
+                  // Maak een tabel om de resultaten weer te geven
+                  echo "<table id='myTable'>";
+                  echo "<tr class='header'><th>Naam</th><th>Opdracht</th><th>Totale tijd</th></tr>";
+                  
+                  // Output data van elke rij
+                  while($row = $result->fetch_assoc()) {
+                    echo "<tr><td>" . $row["userFirstname"]. " " . $row["userLastname"]. "</td><td>" . $row["assignmentName"]. "</td><td>" . $row["totalTime"]. "</td></tr>";
+                  }
+                  echo "</table>";
+                } else {
+                  echo "0 resultaten";
+                }
+                ?>
             </div>
         </div>
     </main>
 </body>
 
 </html>
+
