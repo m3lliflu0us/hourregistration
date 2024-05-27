15b2e7e67b5316a16c069e3f1cd1b66367b9a0d2
Maso44 <143696681+Maso44@users.noreply.github.com>
2024-05-27 14:27:24 +0200

    Update generate_invoice.php

--- client/generate_invoice.php
+++ client/generate_invoice.php
@@ -14,10 +14,8 @@ $currentPage = 'invoice';
 include("../db/dbh.inc.php");
 require('../fpdf186/fpdf.php');
 
-// Get assignmentId from POST data
 $assignmentId = $_POST['assignmentId'];
 
-// Query to get data
 $query = "
 SELECT a.activityId, a.totalTime, u.userFirstname, u.userLastname, c.clientFirstname, c.clientLastname, c.companyName, c.companyAddress, asgn.assignmentName,
     (SELECT SUM(a.totalTime)
@@ -38,13 +36,10 @@ $pdf->SetMargins(0, 0, 0);
 
 $var_id_facture = $assignmentId;
 
-// Generate the PDF
 $pdf->AddPage();
 
-// Logo
 $pdf->Image('../logo-test.png', 10, 10, 80, 55);
 
-// Page number
 $pdf->SetXY(120, 5);
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(160, 8, '1/1', 0, 0, 'R');
@@ -101,7 +96,6 @@ $pdf->SetXY($x, $y);
 $pdf->SetFont('Arial', '', 10);
 $pdf->Cell(100, 8, $receiver_address, 0, 0, '');
 
-// Sender Information
 $pdf->SetXY(5, $y - 11); 
 $pdf->SetFont('Arial', 'B', 11);
 $pdf->Cell(100, 8, "MIJN BEDRIJF:", 0, 0, '');
@@ -118,9 +112,8 @@ $pdf->SetXY(5, $y - 11);
 $pdf->SetFont('Arial', '', 10);
 $pdf->Cell(100, 8, $sender_address, 0, 0, '');
 
-// Articles table header
 $pdf->SetLineWidth(0.2);
-$pdf->SetDrawColor(255, 255, 255); // Set the border color to white
+$pdf->SetDrawColor(255, 255, 255); 
 $pdf->Rect(5, 95, 200, 118, "D");
 $pdf->Line(5, 105, 205, 105);
 $pdf->Line(145, 95, 145, 213);
@@ -128,8 +121,7 @@ $pdf->Line(158, 95, 158, 213);
 $pdf->Line(176, 95, 176, 213);
 $pdf->Line(187, 95, 187, 213);
 
-// Add black line under "Formulering" header
-$pdf->SetDrawColor(0, 0, 0); // Set the border color to black
+$pdf->SetDrawColor(0, 0, 0); 
 $pdf->Line(5, 103, 205, 103);
 
 $pdf->SetXY(1, 96);
@@ -169,7 +161,6 @@ while ($row = $result->fetch_assoc()) {
     $TotalMoneyWithBTW = $TotalMoney * 1.21;
     $btw = $TotalMoneyWithBTW - $TotalMoney;
 
-    // Format monetary values
     $formattedTotalMoney = number_format($TotalMoney, 2, ',', '.');
     $formattedTotalMoneyWithBTW = number_format($TotalMoneyWithBTW, 2, ',', '.');
 
@@ -192,35 +183,28 @@ while ($row = $result->fetch_assoc()) {
     $y += 6;
 }
 
-// Draw a line under the last row
 $finalY = $y + 9;
 $pdf->SetXY(5, $finalY);
-$pdf->Cell(200, 0, '', 'T'); // Draw a horizontal line
+$pdf->Cell(200, 0, '', 'T'); 
 
-// Format and display the total amount
 $formattedTotaal = number_format($totaal, 2, ',', '.');
 $formattedBTW = number_format($totaal * 0.21, 2, ',', '.');
 $totalPrice = number_format($totaal * 1.21, 2, ',', '.');
-
-// Display the Subtotaal
 
 $pdf->SetXY(155, $y + 9);
 $pdf->Cell(-1, 7, "Subtotaal", 0, 0, 'R'); 
 $pdf->SetXY(177, $y + 9);
 $pdf->Cell(10, 7, EURO . $formattedTotaal, 0, 0, 'R'); 
 
-// Add the line under the BTW
 $pdf->SetDrawColor(0, 0, 0);
 $pdf->Line(155, $y + 23, 205, $y + 23);
 
-// Display the 21% BTW
 
 $pdf->SetXY(155, $y + 17);
 $pdf->Cell(-1, 5, "BTW(21%)", 0, 0, 'R'); 
 $pdf->SetXY(177, $y + 17);
 $pdf->Cell(10, 5, EURO . $formattedBTW, 0, 0, 'R'); 
 
-// Display the Totaal Bedrag
 $totalAmount = $totaal * 1.21;
 $formattedTotalAmount = number_format($totalAmount, 2, ',', '.');
 
@@ -230,7 +214,7 @@ $pdf->SetXY(177, $y + 25);
 $pdf->Cell(10, 7, EURO . $formattedTotalAmount, 0, 0, 'R');
 
 $pdf->SetFont('Arial', '', 10);
-$pdf->SetXY(10, 282); // Adjust the coordinates as needed
+$pdf->SetXY(10, 282); 
 $pdf->Cell(0, 0, 'Betaling binnen 14 dagen op IBAN NL12 RABO 3456 789 10, onder vermelding van het factuurnummer.', 0, 1, 'L');
 
 $pdf->Output();
